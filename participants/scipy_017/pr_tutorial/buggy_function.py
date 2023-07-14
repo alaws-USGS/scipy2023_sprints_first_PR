b876aba8e56f5a9abd464f2226d01eab14b5e23b
laws-spatial <alaws@contractor.usgs.gov>
2023-07-14 17:39:48 -0500

    FIX: buggy function

--- participants/scipy_017/pr_tutorial/buggy_function.py
+++ participants/scipy_017/pr_tutorial/buggy_function.py
@@ -18,15 +18,21 @@ def angle_to_sexigesimal(angle_in_degrees: float, decimals:int =3):
 
     """
     if type(decimals) != int:
-        raise OSError('decimals should be an integer!')
+        raise TypeError('decimals should be an integer!')
 
-    hours_num = angle_in_degrees*24/180
+    hours_num = angle_in_degrees*24/360
     hours = math.floor(hours_num)
+    str_hours = str(hours).zfill(2)
 
     min_num = (hours_num - hours)*60
     minutes = math.floor(min_num)
+    str_minutes = str(minutes).zfill(2)
 
-    seconds = (min_num - minutes)*60
+    seconds = round((min_num - minutes)*60, 2)
+    str_seconds = str(seconds).zfill(2)
 
-    format_string = '{}:{}:{:.' + str(decimals) + 'f}'
-    return format_string.format(hours, minutes, seconds)
+    format_string = f"{str_hours}:{str_minutes}:{str_seconds}"
+    return format_string
+
+test_angle = angle_to_sexigesimal(89)
+print(test_angle)
