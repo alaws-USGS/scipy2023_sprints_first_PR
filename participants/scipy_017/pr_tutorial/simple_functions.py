95a8af7b5afe6f4a8878f9b60642731633c2deec
laws-spatial <alaws@contractor.usgs.gov>
2023-07-14 17:15:33 -0500

    FEAT: added is_prime function to simple_functions.py

--- participants/scipy_017/pr_tutorial/simple_functions.py
+++ participants/scipy_017/pr_tutorial/simple_functions.py
@@ -10,3 +10,19 @@ def factorial(value):
         return 1
     else:
         return value * factorial(value - 1)
+
+def is_prime(value):
+    if value == 1:
+        print(value, "is not a prime number")
+    elif value > 1:
+    # check for factors
+        for i in range(2,value):
+            if (value % i) == 0:
+                break
+            else:
+                print(value,"is a prime number")
+        
+    # if input number is less than
+    # or equal to 1, it is not prime
+    else:
+        print(value,"is not a prime number")
